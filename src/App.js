0371d320c85bb79f1099635f4f5392e6372b818b
VanHau87 <nguyenvanh29@gmail.com>
2023-09-27 22:30:17 +0700

    setup json-server and first use useReducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,54 @@
-import DateCounter from "./DateCounter";
+import { useEffect } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import { useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  status: "loading", //error, ready, active, finished
+};
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case "received":
+      state = { ...state, status: "ready", questions: payload };
+      break;
+    case "not-found":
+      state = { ...state, status: "Not found" };
+      break;
+    case "error":
+      state = { ...state, status: "failed" };
+      break;
+    default:
+      throw new Error("Action Unknown");
+  }
+  return state;
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch("http://localhost:8000/questios");
+        if (response.status === 404) {
+          dispatch({ type: "not-found" });
+          return;
+        }
+        const data = await response.json();
+        dispatch({ type: "received", payload: data });
+      } catch (error) {
+        dispatch({ type: "error" });
+      }
+    })();
+  }, []);
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main className="main">
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
     </div>
   );
 }
--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,34 +1,55 @@
-import { useState } from "react";
+import { useReducer } from "react";
 
+const reducer = (state, { type, payload }) => {
+  switch (type) {
+    case "def":
+      state = { ...state, count: payload.count };
+      break;
+    case "count":
+      const newCount = state.count + payload.count;
+      state = { ...state, count: newCount };
+      break;
+    case "step":
+      state = { ...state, step: payload.step };
+      break;
+    case "reset":
+      state = { step: 1, count: 0 };
+      break;
+    default:
+      throw new Error("Unknow action");
+  }
+  return state;
+};
 function DateCounter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+  const initialState = { count: 0, step: 1 };
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
-  date.setDate(date.getDate() + count);
+  // date.setDat e(date.getDate() + count);
 
   const dec = function () {
-    // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    dispatch({ type: "count", payload: { count: -state.step } });
   };
 
   const inc = function () {
-    // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    dispatch({ type: "count", payload: { count: state.step } });
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    const value = Number(e.target.value);
+    const number = !isNaN(value) ? value : 0;
+    const obj = { type: "def", payload: { count: number } };
+    dispatch(obj);
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    const value = Number(e.target.value);
+    dispatch({ type: "step", payload: { step: value } });
   };
 
   const reset = function () {
-    setCount(0);
-    setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
@@ -38,15 +59,15 @@ function DateCounter() {
           type="range"
           min="0"
           max="10"
-          value={step}
+          value={state.step}
           onChange={defineStep}
         />
-        <span>{step}</span>
+        <span>{state.step}</span>
       </div>
 
       <div>
         <button onClick={dec}>-</button>
-        <input value={count} onChange={defineCount} />
+        <input value={state.count} onChange={defineCount} />
         <button onClick={inc}>+</button>
       </div>
 
